fe9dc7e36c56c3ef73251771a97ca9183a3ebeb4
theSummit12 <46876350+theSummit12@users.noreply.github.com>
2021-02-25 13:29:55 -0600

    atted write_file and open_file

--- file_opener.py
+++ file_opener.py
@@ -4,17 +4,23 @@ import tkinter.scrolledtext as st
 
 root = Tk()
 root.title('A Random Gui')
-root.geometry('200x200')
+root.geometry('300x200')
 root.configure(background='grey38')
+text_box = ''
 
 def close_file():
     print('Closing File')
+    close_file_name = filedialog.asksaveasfilename(title='Please select a file to close...', filetypes=(("txt files", "*txt"), ('all files', '*.*')))
+    print('Saving to' + close_file_name)
+    write_file(close_file_name)
+
 
 
 def open_file():
     print('open file')
-    open_file_name = filedialog.askopenfilename(title='Please select a file to open...', filetypes=(("txt files", "w.txt"), ('all files', 'w.w')))
+    open_file_name = filedialog.askopenfilename(title='Please select a file to open...', filetypes=(("txt files", "*txt"), ('all files', '*.*')))
     print('Opening file at'+open_file_name)
+    read_file(open_file_name)
 
 def submenu():
     menu = Menu(root)
@@ -23,27 +29,32 @@ def submenu():
     menu.add_cascade(label="File", menu=submenu)
     submenu.add_command(label="Open", command=open_file)
     submenu.add_command(label='Close', command=close_file)
-#    submenu.add_seperator()
+#   
 
-def show_text_box():
+def show_text_box(text):
+    global text_box 
     print('show_text_box')
     text_box = st.ScrolledText(root, bg='white', font='TkFixedFont')
-    text_box.insert(INSERT,'inserting')
+    text_box.insert(INSERT, text)
     text_box.place(x=100,y=100,width=100,height=100)
 
-
-def read_file():
+def read_file(file_path):
     print('read_file')
+    file = open(file_path)
+    text = ''
+    for line in file:
+        text = text + line 
+    show_text_box(text)
 
 
-def write_file():
+def write_file(close_file_path):
+    get_text_box = text_box.get('1.0', 'end-1c')
     print('write_file')
+    print(close_file_path)
+    new_file = open(close_file_path, 'w')
+    new_file.writelines(get_text_box)
 
 
-# simple buttons for UI 
-Button(root, text='Show text box', command=show_text_box, bg='grey',highlightbackground='black',foreground='dark blue').grid(row=0,column=0)
-Button(root, text='Get Text', command=read_file, bg='grey',highlightbackground='black',foreground='dark blue').grid(row=0,column=1)
-Button(root, text='Write Text', command=write_file, bg='grey',highlightbackground='black',foreground='dark blue').grid(row=0,column=2)
 
 
 submenu()
